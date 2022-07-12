3566262220af8d26632ace891d1c601588664dda
glabular <maxmail.com@ya.ru>
2022-07-12 16:05:52 +0400

    Added new set of rules for life (max 4 possible neighbours).

--- Program.cs
+++ Program.cs
@@ -72,6 +72,40 @@ namespace ConwayLife
             return temporaryStateOfField;
         }
 
+        public static CellStatus[,] SurviveDieOrBorn4(
+            int totalRows,
+            int totalColumns,
+            CellStatus[,] currentStateOfField,
+            Func<int, int, int, int, CellStatus[,], int> getNumberOfNeighbours)
+        {
+            CellStatus[,] temporaryStateOfField = new CellStatus[totalRows, totalColumns];
+
+            for (int currentRowIndex = 0; currentRowIndex < totalRows; currentRowIndex++)
+            {
+                for (int currentColumnIndex = 0; currentColumnIndex < totalColumns; currentColumnIndex++)
+                {
+                    var numberOfNeighbours = getNumberOfNeighbours(currentRowIndex, currentColumnIndex, totalRows, totalColumns, currentStateOfField);
+
+                    switch (numberOfNeighbours)
+                    {
+                        case 3: // Если у клетки 3 соседа
+                            // Оставить состояние клетки таким же, как и было.
+                            temporaryStateOfField[currentRowIndex, currentColumnIndex] = currentStateOfField[currentRowIndex, currentColumnIndex];
+                            break;
+                        case 2: // Если у клетки 2 соседа
+                            // Сделать или оставить клетку живой.
+                            temporaryStateOfField[currentRowIndex, currentColumnIndex] = CellStatus.Alive;
+                            break;
+                        default: // Во всех остальных случаях - убить клетку или оставить её пустой.
+                            temporaryStateOfField[currentRowIndex, currentColumnIndex] = CellStatus.Empty;
+                            break;
+                    }
+                }
+            }
+
+            return temporaryStateOfField;
+        }
+
         private static int GetNeighboursNumber8(int currentRowIndex, int currentColumnIndex, int totalRows, int totalColumns, CellStatus[,] currentStateOfField)
         {
             int neighbours = 0;
@@ -100,6 +134,56 @@ namespace ConwayLife
             bool IsPointValid(PointOnBoard scanPoint)
             {
                 return !(scanPoint.Column == currentColumnIndex && scanPoint.Row == currentRowIndex);
+            }
+        }
+
+        private static int GetNeighboursNumber4(int currentRowIndex, int currentColumnIndex, int totalRows, int totalColumns, CellStatus[,] currentStateOfField)
+        {
+            int neighbours = 0;
+
+            if (HasNeighbourRow(-1))
+            {
+                neighbours++;
+            }
+
+            if (HasNeighbourRow(1))
+            {
+                neighbours++;
+            }
+
+            if (HasNeighbourColumn(-1))
+            {
+                neighbours++;
+            }
+
+            if (HasNeighbourColumn(1))
+            {
+                neighbours++;
+            }
+
+
+            return neighbours;
+
+            bool HasNeighbourRow(int index)
+            {
+                var scanPoint = new PointOnBoard
+                {
+                    Column = (currentColumnIndex + index + totalColumns) % totalColumns,
+                    Row = currentRowIndex
+                };
+
+                return currentStateOfField[scanPoint.Row, scanPoint.Column] != CellStatus.Empty;
+            }
+
+            bool HasNeighbourColumn(int index)
+            {
+                var scanPoint = new PointOnBoard
+                {
+                    Column = currentColumnIndex,
+                    Row = (currentRowIndex + index + totalRows) % totalRows
+                };
+
+                return currentStateOfField[scanPoint.Row, scanPoint.Column] != CellStatus.Empty;
             }
         }
     }
